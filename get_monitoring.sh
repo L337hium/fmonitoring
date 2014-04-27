4e33022d68fde87f94e7d7c6e183510453dd44a2
ed <bernd@kr217.de>
2014-04-27 20:04:50 +0200

    intent & network clients

--- get_monitoring.sh
+++ get_monitoring.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-
 if [[ -z "$*" ]]; then
 	__PARAMS="system gps olsr network"
 else
@@ -13,32 +12,29 @@ for __PARAM in $__PARAMS; do
 			OUT=$OUT', "system": '$(sh get_monitoring_system.sh)
 			;;
 		"gps")	
-			. /usr/share/libubox/jshn.sh
-			json_init
-			json_add_string "latitude" "$( uci get meshwizard.system.latitude )"
-			json_add_string "longitude" "$( uci get meshwizard.system.longitude )"
-			json_add_string "location" "$( uci get meshwizard.system.location )"
+				. /usr/share/libubox/jshn.sh
+				json_init
+				json_add_string "latitude" "$( uci get meshwizard.system.latitude )"
+				json_add_string "longitude" "$( uci get meshwizard.system.longitude )"
+				json_add_string "location" "$( uci get meshwizard.system.location )"
 
 			OUT=$OUT', "gps": '$( json_dump )
 			;;
 		"olsr")	
 			OUT=$OUT', "olsr": { '
-			OUT=$OUT'"ip": "'$( uci get network.wlanadhoc.ipaddr )'", '
-			OUT=$OUT'"hostname": "'$( uci get meshwizard.system.hostname )'", '
-			OUT=$OUT'"neighbors": '$( echo /links | nc localhost 9090 )
+				OUT=$OUT'"ip": "'$( uci get network.wlanadhoc.ipaddr )'", '
+				OUT=$OUT'"hostname": "'$( uci get meshwizard.system.hostname )'", '
+				OUT=$OUT'"neighbors": '$( echo /links | nc localhost 9090 )
 			OUT=$OUT' }'
 			;;
 		"network")
-			#ubus -vS call hostapd.wlan0 get_clients
 			NETWORK_DEVICES="wan lan wlan wlanadhoc"
 
 			OUT=$OUT', "network": { '
-			local j=0
-			for DEV in $NETWORK_DEVICES; do
-				[[ $j -gt 0 ]] && OUT=$OUT", "
-				OUT=$OUT' "'$DEV'": '$( sh get_monitoring_network.sh $DEV )
-				j=$( expr $j + 1 )
-			done
+				OUT=$OUT'"clients": '$( cat /var/dhcp.leases | wc -l )
+				for DEV in $NETWORK_DEVICES; do
+					OUT=$OUT', "'$DEV'": '$( sh get_monitoring_network.sh $DEV )
+				done
 			OUT=$OUT' }'
 			;;
 		*)
@@ -46,6 +42,5 @@ for __PARAM in $__PARAMS; do
 	esac
 done
 OUT=$OUT' }'
-
 echo $OUT
 exit 0
