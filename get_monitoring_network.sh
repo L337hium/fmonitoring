84b0a404176fd7705744c05d03402ec6343e2e5f
ed <bernd@kr217.de>
2014-04-30 17:52:22 +0200

    no more ifconfig

--- get_monitoring_network.sh
+++ get_monitoring_network.sh
@@ -12,17 +12,17 @@ json_get_var MACADDR macaddr
 json_get_var UP up
 json_get_var LINK link
 json_select statistics
-if [[ "$DEV" == "wlan" -o "$DEV" == "wlanadhoc" ]]; then
-	RX_BYTES=$( ifconfig $(uci get network.$DEV.ifname) | grep "RX bytes" | sed 's/.*RX bytes:\([0-9]*\).*/\1/' )
-	RX_PACKETS=$( ifconfig $(uci get network.$DEV.ifname) | grep "RX packets" | sed 's/.*RX packets:\([0-9]*\).*/\1/' )
-	TX_BYTES=$( ifconfig $(uci get network.$DEV.ifname) | grep "TX bytes" | sed 's/.*TX bytes:\([0-9]*\).*/\1/' )
-	TX_PACKETS=$( ifconfig $(uci get network.$DEV.ifname) | grep "TX packets" | sed 's/.*TX packets:\([0-9]*\).*/\1/' )
-else
+#if [[ "$DEV" == "wlan" -o "$DEV" == "wlanadhoc" ]]; then
+#	RX_BYTES=$( ifconfig $(uci get network.$DEV.ifname) | grep "RX bytes" | sed 's/.*RX bytes:\([0-9]*\).*/\1/' )
+#	RX_PACKETS=$( ifconfig $(uci get network.$DEV.ifname) | grep "RX packets" | sed 's/.*RX packets:\([0-9]*\).*/\1/' )
+#	TX_BYTES=$( ifconfig $(uci get network.$DEV.ifname) | grep "TX bytes" | sed 's/.*TX bytes:\([0-9]*\).*/\1/' )
+#	TX_PACKETS=$( ifconfig $(uci get network.$DEV.ifname) | grep "TX packets" | sed 's/.*TX packets:\([0-9]*\).*/\1/' )
+#else
 	json_get_var RX_BYTES rx_bytes
 	json_get_var RX_PACKETS rx_packets
 	json_get_var TX_BYTES tx_bytes
 	json_get_var TX_PACKETS tx_packets
-fi
+#fi
 unset $TMP
 
 build_json(){
@@ -40,3 +40,6 @@ build_json(){
 
 build_json
 exit 0
+
+# TODO: Use `ifstatus` ! ! !
+# avoid 
