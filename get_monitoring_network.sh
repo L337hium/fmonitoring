097b45690392bcd980d92b56fc4d43acd194b164
ed <bernd@kr217.de>
2014-05-03 16:28:15 +0200

    use ifstatus and not devstatus

--- get_monitoring_network.sh
+++ get_monitoring_network.sh
@@ -2,28 +2,18 @@
 # $1 = wan | lan | wlan | wlanadhoc
 [[ -z $1 ]] && exit 1
 
-DEV="$1"
-local TMP=$( devstatus $( uci get network.$DEV.ifname ) )
-[[ "$TMP" == "Usage: /sbin/devstatus <device>" ]] && exit 2
+get_device(){
+	. /usr/share/libubox/jshn.sh
+	json_load "$( ifstatus $1 )"
+	json_get_var DEVICE device
+	echo $DEVICE
+}
 
-. /usr/share/libubox/jshn.sh
-json_load "$TMP"
-json_get_var MACADDR macaddr
-json_get_var UP up
-json_get_var LINK link
-json_select statistics
-#if [[ "$DEV" == "wlan" -o "$DEV" == "wlanadhoc" ]]; then
-#	RX_BYTES=$( ifconfig $(uci get network.$DEV.ifname) | grep "RX bytes" | sed 's/.*RX bytes:\([0-9]*\).*/\1/' )
-#	RX_PACKETS=$( ifconfig $(uci get network.$DEV.ifname) | grep "RX packets" | sed 's/.*RX packets:\([0-9]*\).*/\1/' )
-#	TX_BYTES=$( ifconfig $(uci get network.$DEV.ifname) | grep "TX bytes" | sed 's/.*TX bytes:\([0-9]*\).*/\1/' )
-#	TX_PACKETS=$( ifconfig $(uci get network.$DEV.ifname) | grep "TX packets" | sed 's/.*TX packets:\([0-9]*\).*/\1/' )
-#else
-	json_get_var RX_BYTES rx_bytes
-	json_get_var RX_PACKETS rx_packets
-	json_get_var TX_BYTES tx_bytes
-	json_get_var TX_PACKETS tx_packets
-#fi
-unset $TMP
+get_devstatus(){
+	. /usr/share/libubox/jshn.sh
+	json_load "$( devstatus $( get_device $1 ) )"
+	echo $( json_dump )
+}
 
 build_json(){
 	. /usr/share/libubox/jshn.sh
@@ -35,11 +25,19 @@ build_json(){
 	json_add_int "rx_packets" "$RX_PACKETS"
 	json_add_int "tx_bytes" "$TX_BYTES"
 	json_add_int "tx_packets" "$TX_PACKETS"
-	echo $(json_dump)
+	echo $( json_dump )
 }
+
+. /usr/share/libubox/jshn.sh
+json_load "$( get_devstatus $1 )"
+json_get_var MACADDR macaddr
+json_get_var UP up
+json_get_var LINK link
+json_select statistics
+json_get_var RX_BYTES rx_bytes
+json_get_var RX_PACKETS rx_packets
+json_get_var TX_BYTES tx_bytes
+json_get_var TX_PACKETS tx_packets
 
 build_json
 exit 0
-
-# TODO: Use `ifstatus` ! ! !
-# avoid 
